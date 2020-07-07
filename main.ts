afe672082982ceeae5c7266f2164a46a7de0a23a
Isurii <isuri.yahampath2@gmail.com>
2020-07-07 11:24:39 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let a = 2
+let b = 3
+let c = a + b
+basic.showNumber(c)
+if (c < 10) {
+    basic.showIcon(IconNames.Yes)
+} else {
+    basic.showIcon(IconNames.No)
+}
+basic.forever(function () {
+	
+})
